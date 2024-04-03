a304eac91f36c5b070df923384e2975b7e5c474e
Kainat-Iqbal <kainatjiq@gmail.com>
2024-04-04 00:26:20 +0500

    start working on main screen

---
+++ src/Components/MainScreen/index.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import './style.css';
+
+function MainScreen(){
+    return(
+        <div id="mainDivMainScreen">
+            <div id="top">
+
+            </div>
+
+            <div id="middle">
+
+            </div>
+
+            <div id="bottom">
+
+            </div>
+
+        </div>
+    )
+}
+export default MainScreen;
