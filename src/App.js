9635dfab9a469773dec6bf560bbf47021c4ab4a3
Kainat-Iqbal <kainatjiq@gmail.com>
2024-04-06 21:01:28 +0500

    landing page and login completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import MainScreen from './Components/MainScreen';
+import MainScreen from './Components/LandingPage/MainScreen';
+import LoginPage from './Components/LandingPage/LoginPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      <MainScreen/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainScreen/>}/>
+
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
