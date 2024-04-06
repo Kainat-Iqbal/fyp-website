ea7999d1ee348cdaa0a0483ba0dfb3a96959d1c9
Kainat-Iqbal <kainatjiq@gmail.com>
2024-04-07 01:52:15 +0500

    start working on admin panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import MainScreen from './Components/LandingPage/MainScreen';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      <MainScreen/>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainScreen/>}/>
+
+        <Route path='/login' element={<LoginPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Components/AdminPanel/Home/index.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import SideBar from "../SideBar";
+import "./style.css";
+
+function Home() {
+  return(
+  <div id="HomeMainDiv">
+    <div id="homeTop"></div>
+    <SideBar />
+  </div>
+  )
+}
+export default Home;
